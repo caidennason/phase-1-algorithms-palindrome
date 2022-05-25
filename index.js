ac714e5993d32c86b33ffd6dce761845446c1151
caiden nason <caidenbnason@gmail.com>
2022-05-25 13:51:00 -0700

    commit tree 488bc74fc4e09f212fea09892f5490784a8ef121

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let arr = []
+    let newWord = word.split('')
+    for (i = word.length; i >= 0; i--){
+        arr.push(newWord[i])
+        let otherWord = arr.join('')
+        //if (otherWord === word)
+        //return true 
+        if (otherWord === word){
+          return true
+    }
+  }
+  return false
+};
+
+
 
 /* 
-  Add your pseudocode here
+  get each part of the word
+  reverse the word, creating a new word
+  if the new word is equal to the old word
+  return true
+
+  splitting word into letters
+  iterate through array starting from the back
+  push each letter into a new array
+  join letters together
 */
 
 /*
